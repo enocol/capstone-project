4f99b8f9020e8414384c4c3ab1febbfa1092d3b4
Enoh <eno.col@gmail.com>
2023-08-09 08:50:28 +0100

    Edit .js files, .html , .css to re-implement the show more button

--- showmore.js
+++ showmore.js
@@ -1,17 +1,24 @@
-const speaker = document.querySelectorAll(".speaker");
-const showmorebtn = document.querySelector(".showmore");
-const itemperload = 2;
-const currentVisible = itemperload;
+document.addEventListener("DOMContentLoaded", () => {
+  const speaker = document.querySelectorAll(".speaker");
+  console.log(speaker);
+  const showmorebtn = document.querySelector(".showmore");
+  const itemperload = 2;
+  const currentVisible = itemperload;
 
-for (let i = 0; i < currentVisible; i += 1) {
-  speaker[i].classList.add("visible");
-}
-
-showmorebtn.addEventListener("click", () => {
-  for (let i = currentVisible; i < currentVisible + itemperload; i += 1) {
-    if (speaker[i]) {
-      speaker[i].classList.add("visible");
-    }
+  for (let i = 0; i < currentVisible; i += 1) {
+    speaker[i].classList.add("visible");
   }
-  showmorebtn.classList.add("btnInvisible");
+
+  showmorebtn.addEventListener("click", () => {
+    for (
+      let i = currentVisible;
+      i <= currentVisible + itemperload + 1;
+      i += 1
+    ) {
+      if (speaker[i]) {
+        speaker[i].classList.add("visible");
+      }
+    }
+    showmorebtn.classList.add("btnInvisible");
+  });
 });
